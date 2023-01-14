26b78d6ec461481f38107a122af4ba2b21d86e7b
durgaprasad.mohite <mohited24@gmail.com>
2023-01-15 00:57:22 +0530

    End of section 2

---
+++ NZWalks.API/Migrations/20230114133945_InitialMigration.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Regions",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Area = table.Column<double>(type: "float", nullable: false),
+                    Lat = table.Column<double>(type: "float", nullable: false),
+                    Long = table.Column<double>(type: "float", nullable: false),
+                    Population = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Regions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "WalksDifficulty",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WalksDifficulty", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Walks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Length = table.Column<double>(type: "float", nullable: false),
+                    RegionID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WalkDifficultyID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    WalksDifficultyId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Walks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Walks_Regions_RegionID",
+                        column: x => x.RegionID,
+                        principalTable: "Regions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Walks_WalksDifficulty_WalksDifficultyId",
+                        column: x => x.WalksDifficultyId,
+                        principalTable: "WalksDifficulty",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Walks_RegionID",
+                table: "Walks",
+                column: "RegionID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Walks_WalksDifficultyId",
+                table: "Walks",
+                column: "WalksDifficultyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Walks");
+
+            migrationBuilder.DropTable(
+                name: "Regions");
+
+            migrationBuilder.DropTable(
+                name: "WalksDifficulty");
+        }
+    }
+}
---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,16 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionID { get; set; }
+        public Guid WalksDifficultyID { get; set; }
+
+        // Navigation properties
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+
+    }
+}
