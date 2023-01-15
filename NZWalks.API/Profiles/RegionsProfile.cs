4871ab21be5453b5f499cdec4fac7f616d012a9b
durgaprasad.mohite <mohited24@gmail.com>
2023-01-15 14:32:15 +0530

    End of Section 3

---
+++ NZWalks.API/Profiles/RegionsProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+namespace NZWalks.API.Profiles
+{
+    public class RegionsProfile : Profile
+    {
+        public RegionsProfile()
+        {
+            CreateMap<Models.Domain.Region, Models.DTO.Region>();
+                //if property names are different then need to do like below
+                //.ForMember(dest=>dest.Id, options => options.MapFrom(src => src.Id));
+
+                //reverse map is used to convert DTO to model
+                //.ReverseMap();
+
+
+        }
+    }
+}
